37d3fa02c54c478e131742736d8699e538172166
OLIVER DEL JESÚS BÁEZ <OADB0001@CE.PUCMM.EDU.DO>
2020-12-08 01:24:08 +0100

    Agregar archivos de proyecto.

---
+++ mortal kombat/Game1.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace mortal_kombat
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D fondo;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.IsFullScreen = false;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            fondo = Content.Load<Texture2D>("tn");
+            
+        }
+            
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw
+             (fondo, new Rectangle(0, 0,712,
+             
+             385),
+              Color.White);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+            
+        }
+    }
+}
